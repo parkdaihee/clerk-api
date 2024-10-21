edc0ea5971a1a14172864d3ae2153e005fba4bdb
parkdaihee <162399285+parkdaihee@users.noreply.github.com>
2024-10-21 23:17:47 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function Example() {
   return (
     <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -5,7 +7,7 @@ export default function Example() {
       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
         <img
-          alt="로고"
+          alt=""
           src="https://tailwindui.com/plus/img/logos/workcation-logo-indigo-600.svg"
           className="mx-auto h-12"
         />
@@ -14,14 +16,16 @@ export default function Example() {
             <p>
               안녕하세요! 박대희의 홈페이지에 오신 것을 환영합니다. 이곳은 저의
               다양한 프로젝트와 작업을 소개하는 공간입니다. 함께하는 모든 순간이
-              소중하며, 여러분의 방문을 진심으로 감사합니다.
+              소중하며, 여러분의 방문을 진심으로 감사합니다.&apos;
             </p>
           </blockquote>
           <figcaption className="mt-10">
-            <img
-              alt="박대희"
-              src="/박대희.png" // public 폴더 내의 이미지 경로
+            <Image
+              alt=""
+              src="/박대희.png" // 이미지 경로
               className="mx-auto h-10 w-10 rounded-full"
+              width={40} // 원하는 너비
+              height={40} // 원하는 높이
             />
             <div className="mt-4 flex items-center justify-center space-x-3 text-base">
               <div className="font-semibold text-gray-900">Park Daihee</div>
