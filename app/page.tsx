9c07f2528bcb0632c659ede648015d750ff5fecf
parkdaihee <park827401@naver.com>
2024-10-22 01:29:25 +0900

    df

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image' // next/image 임포트
 
 function Homepage() {
   return (
@@ -21,7 +20,7 @@ function Homepage() {
       }}
     >
       <h1 style={{ fontSize: '2em', marginBottom: '10px', color: 'white' }}>
-        Park Daihee&apos;s Homepage
+        Park Daihee Homepage
       </h1>
       <p style={{ fontSize: '1.5em', marginBottom: '20px', color: 'white' }}>
         안녕하세요! 박대희의 홈페이지에 오신 것을 환영합니다.
@@ -31,14 +30,6 @@ function Homepage() {
         이곳은 저의 다양한 프로젝트와 작업을 소개하는 공간입니다. 함께하는 모든
         순간이 소중하며, 여러분의 방문을 진심으로 감사합니다.
       </p>
-      {/* GIF 이미지 사용 시, next/image 사용 */}
-      <Image
-        src="/water2.gif" // GIF 이미지 경로
-        alt="Water background" // 대체 텍스트
-        layout="fill" // 채우기 레이아웃
-        objectFit="cover" // 이미지 커버
-        quality={100} // 품질 설정
-      />
     </div>
   )
 }
